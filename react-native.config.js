9951da3fa6fc5d91c985211df6303e0a7402ab83
shivam <trickloot8.1@gmail.com>
2025-04-07 08:27:17 +0530

    add vector icons

--- react-native.config.js
+++ react-native.config.js
@@ -3,5 +3,12 @@ module.exports = {
     ios: {},
     android: {},
   },
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null, // Disable auto-linking for iOS to avoid font duplication issues
+      },
+    },
+  },
   assets: ['./src/assets/fonts/'],
 };
