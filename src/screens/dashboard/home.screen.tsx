a0210f112f4eb072e4f6e111e99e717bec130c3b
shivam <trickloot8.1@gmail.com>
2025-04-12 08:36:09 +0530

    home desing imprved

--- src/screens/dashboard/home.screen.tsx
+++ src/screens/dashboard/home.screen.tsx
@@ -7,6 +7,27 @@ export function HomeScreen() {
     image: 'https://marketplace.canva.com/EAFaFUz4aKo/3/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-tn1zF-_cG9c.jpg',
   };
 
+  const options = [
+    {
+      name: 'Math Quiz',
+      route: '/quiz/math',
+      image: 'https://example.com/math-quiz.png',
+      question: Math.floor(Math.random() * (40 - 3 + 1)) + 3,
+    },
+    {
+      name: 'Science Trivia',
+      route: '/quiz/science',
+      image: 'https://example.com/science-trivia.png',
+      question: Math.floor(Math.random() * (40 - 3 + 1)) + 3,
+    },
+    {
+      name: 'History Challenge',
+      route: '/quiz/history',
+      image: 'https://example.com/history-challenge.png',
+      question: Math.floor(Math.random() * (40 - 3 + 1)) + 3,
+    },
+  ];
+
   return (
     <View className="px-4 py-4 ">
       <View className="flex flex-row justify-between">
@@ -17,34 +38,39 @@ export function HomeScreen() {
         <Image className="h-16 w-16 rounded-full" source={{uri: user.image}} />
       </View>
 
-      <View className="mt-4 w-full px-4 mx-auto flex flex-row p-2 justify-between rounded-xl bg-theme ">
-        <View className="flex-row">
+      <View className="mt-5 w-full px-4 mx-auto flex flex-row p-4 justify-evenly rounded-xl shadow-black shadow-2xl border ">
+        <View className="flex-row items-center">
           <Image className="w-10 h-10" source={require('@assets/images/trophy.png')} resizeMode="contain" />
-          <View className='pl-3 flex justify-center'>
-            <Text className='font-interBold text-white'>Ranking</Text>
-            <Text className='font-interBold text-white'>100</Text>
+          <View className="pl-3 flex justify-center">
+            <Text>Ranking</Text>
+            <Text className="font-interBold text-theme ">100</Text>
           </View>
         </View>
-        <View className="w-px bg-white mx-4" />
-        <Image className="w-10 h-10" source={require('@assets/images/coin.png')} resizeMode="contain" />
+
+        <View className="w-px bg-theme-900 mx-4" />
+
+        <View className="flex-row  flex-start items-center">
+          <Image className="w-10 h-10" source={require('@assets/images/coin.png')} resizeMode="contain" />
+          <View className="pl-3 flex justify-center">
+            <Text>Points</Text>
+            <Text className="font-interBold text-theme ">100</Text>
+          </View>
+        </View>
       </View>
 
-      <View className="mt-4">
-        <Text>Let's Play</Text>
-        {/* <View className="flex flex-row justify-around mt-4">
-          <View className="items-center">
-            <Image className="w-12 h-12 mb-2" source={require('@assets/images/book.png')} />
-            <Text>Learn</Text>
-          </View>
-          <View className="items-center">
-            <Image className="w-12 h-12 mb-2" source={require('@assets/images/game.png')} />
-            <Text>Play</Text>
-          </View>
-          <View className="items-center">
-            <Image className="w-12 h-12 mb-2" source={require('@assets/images/chat.png')} />
-            <Text>Chat</Text>
-          </View>
-        </View> */}
+      <View className="mt-6">
+        <Text className="font-interBold text-theme-900 text-xl mb-4">Let's Play</Text>
+        <View className="flex-row flex-wrap gap-3 ">
+          {options.map((currentObject , index) => {
+            return (
+              <View key={index} className="rounded-xl basis-1/2 border p-4">
+                <Image className="w-20 h-20" source={require('@assets/images/coin.png')} resizeMode="contain" />
+                <Text className="mt-2 font-interBold">{currentObject.name}</Text>
+                <Text className="my-1 font-interBold text-xs text-greyish-200">{currentObject.question} Questions</Text>
+              </View>
+            );
+          })}
+        </View>
       </View>
     </View>
   );
