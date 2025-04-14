e7cca729a12b80a66f677403ece9cc529d38a882
shivam <trickloot8.1@gmail.com>
2025-04-14 23:49:44 +0530

    answer box

--- src/screens/dashboard/quiz.screen.tsx
+++ src/screens/dashboard/quiz.screen.tsx
@@ -6,9 +6,9 @@ import colorConstant from '@constant/color.constant';
 
 const QuizScreen: React.FC = () => {
   return (
-    <View className="p-5 justify-around flex-1">
+    <View className="p-5 justify-start flex-1">
       {/* question status indicator starts here */}
-      <View className="flex-row gap-6  px-2 items-center ">
+      <View className="flex-row mt-10 gap-6 px-2 items-center ">
         <Icon name="doubleright" color="black" size={18} className="border-greyish-100 bg-white border font-interBold p-2 rounded-full" />
         <View className="p-3 px-4 border-2 flex-row items-center gap-2 border-greyish-100  rounded-3xl flex-1">
           <View className="bg-theme-900 rounded-xl p-1.5  flex-1"></View>
@@ -18,7 +18,7 @@ const QuizScreen: React.FC = () => {
       {/* question status indicator ends here*/}
 
       {/* question and answer box */}
-      <View>
+      <View className="flex-1 justify-center ">
         <View className="w-full rounded-2xl bg-white  p-8" style={{elevation: 14}}>
           <View className="mx-auto p-2 bg-white rounded-full -mt-20 mb-5">
             {/* ciricular indicator */}
@@ -41,7 +41,7 @@ const QuizScreen: React.FC = () => {
         </View>
 
         {/* answer box starts here */}
-        <View className="options gap-3 mt-5 w-full">
+        <View className="options gap-4 mt-5 w-full">
           <AnswerBox />
           <AnswerBox />
           <AnswerBox />
@@ -56,19 +56,27 @@ const QuizScreen: React.FC = () => {
 export default QuizScreen;
 
 interface AnswerBoxProps {
-  options: string[];
+  status: 'correct' | 'incorrect' | 'idle';
   onSelect: (option: string) => void;
   selectedOption: string | null;
 }
 
-const AnswerBox: React.FC<AnswerBoxProps> = ({options, onSelect, selectedOption}) => {
-  selectedOption = '34';
+const AnswerBox: React.FC<AnswerBoxProps> = ({onSelect, selectedOption, status = 'correct'}) => {
   const option = '434';
+
+  const isSelected = selectedOption === option;
+  // Hex color based on status
+  const statusHexColor = status === 'incorrect' ? '#EF4444' : status === 'correct' ? '#22C55E' : colorConstant.greyish[100]; // Tailwind greyish-100
+  const iconName = status === 'incorrect' ? 'close' : status === 'correct' ? 'check' : null;
+
   return (
-    <View className={`p-4 border rounded-xl  ${selectedOption === option ? 'bg-theme-900 border-theme-900' : 'bg-white border-greyish-100'}`}>
-      <Text className={`font-interBold ${selectedOption === option ? 'text-white' : 'text-greyish-900'}`} onPress={() => onSelect(option)}>
+    <View style={{borderColor:statusHexColor}} className={`p-3 pr-6 flex-row justify-between border-2 rounded-xl `}>
+     
+      <Text className={`font-interBold ${isSelected ? 'text-white' : 'text-greyish-900'}`} onPress={() => onSelect(option)}>
         {option}
       </Text>
+
+      <View style={{borderColor:statusHexColor}} className={`border rounded-full  overflow-hidden `}>{iconName && <Icon style={{backgroundColor:statusHexColor}} className='p-1' name={iconName} color="white" size={19} />}</View>
     </View>
   );
 };
