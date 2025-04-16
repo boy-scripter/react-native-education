d722364576c644af279fcd111853475eb4748c66
shivam <trickloot8.1@gmail.com>
2025-04-16 18:57:41 +0530

    split inpiut for otp

--- src/components/ui/SplitInput.tsx
+++ src/components/ui/SplitInput.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
-import Input from '@components/ui/Input';
-import {View} from 'react-native';
+import React, {useRef} from 'react';
+import {View, TextInput} from 'react-native';
+import {twMerge} from 'tailwind-merge';
+import Input from './Input';
 
 interface SplitInputProps {
+  onSplitChange: (value: string) => void;
   count: number;
+  className?: string;
+  size?: string;
+  inputClassName?: string;
 }
 
-const SplitInput: React.FC<SplitInputProps> = ({count}) => {
+const SplitInput: React.FC<SplitInputProps> = ({count, className, inputClassName, onSplitChange}) => {
+  const inputRefs = useRef<Array<TextInput | null>>([]);
+  const values = useRef<string[]>(Array(count).fill(''));
+
+  const handlePaste = () => {
+    
+  };
+
+  const handleChange = (text: string, index: number) => {
+    values.current[index] = text;
+    onSplitChange(values.current.join(''));
+    // Auto-focus next input if current has text
+    if (text && index < count - 1) {
+      console.log(inputRefs.current[index + 1]);
+      inputRefs.current[index + 1]?.focus();
+    }
+  };
+
+  const handleKeyPress = (e: any, index: number) => {
+    if (e.nativeEvent.key === 'Backspace' && !values.current[index] && index > 0) {
+      inputRefs.current[index - 1]?.focus();
+    }
+  };
+
   return (
-    <View className="flex-row gap-2">
+    <View className={twMerge('flex-row gap-2', className)}>
       {Array.from({length: count}).map((_, index) => (
-        <Input key={index} type="text" maxLength={1} className="border border-gray-300 p-2 px-4 rounded text-center" />
+        <Input
+          key={index}
+          ref={el => {
+            inputRefs.current[index] = el;
+          }}
+          keyboardType="numeric"
+          onChange={text => handleChange(text, index)}
+          onKeyPress={e => handleKeyPress(e, index)}
+          maxLength={1}
+          max={9}
+          placeholder="0"
+          className={twMerge('border border-greyish-100 font-interBold p-2 px-4 w-12 aspect-square rounded text-center', inputClassName)}
+        />
       ))}
     </View>
   );
