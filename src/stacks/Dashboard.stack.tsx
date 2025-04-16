ba1098175149dc8297532b8b7d421924f58d22cd
shivam <trickloot8.1@gmail.com>
2025-04-16 18:59:37 +0530

    new screen

--- src/stacks/Dashboard.stack.tsx
+++ src/stacks/Dashboard.stack.tsx
@@ -3,14 +3,16 @@ import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '@screens/dashboard/home.screen';
 import QuizScreen from '@screens/dashboard/quiz.screen';
 import LeaderBoardScreen from '@screens/dashboard/leaderboard.screen';
+import QuizInstructionScreen from '@screens/dashboard/quizInstruction.screen';
 
 const Stack = createStackNavigator();
 
 const DashboardStack: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName="leaderboard" screenOptions={{headerShown: false}}>
+    <Stack.Navigator initialRouteName="quiz_instruction" screenOptions={{headerShown: false}}>
       <Stack.Screen name="home" component={HomeScreen} />
       <Stack.Screen name="quiz" component={QuizScreen} />
+      <Stack.Screen name="quiz_instruction" component={QuizInstructionScreen} />
       <Stack.Screen name="leaderboard" component={LeaderBoardScreen} />
     </Stack.Navigator>
   );
