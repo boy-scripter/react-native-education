631f429e5ff6c01672fa7a66a109a0c69a00033d
shivam <trickloot8.1@gmail.com>
2025-04-16 15:19:53 +0530

    now more capable

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import {View, TextInput, Text} from 'react-native';
+import {View, TextInput, Text, NativeSyntheticEvent, TextInputKeyPressEventData} from 'react-native';
 import {twMerge} from 'tailwind-merge';
 
 export interface InputProps {
   label?: string;
   name?: string;
-  type: string;
   maxLength?: number;
   max?: number;
   className?: string;
   placeholder?: string;
-  style: Record<any, string>;
+  style?: Record<any, string>;
+  keyboardType?: 'default' | 'email-address' | 'numeric' | 'phone-pad' | 'decimal-pad';
 
+  ref?: React.Ref<TextInput>;
   value?: string;
   onChange?: (text: string) => void;
   onPaste?: (e: React.ClipboardEvent<HTMLInputElement>) => void;
+  onKeyPress?: (e: NativeSyntheticEvent<TextInputKeyPressEventData>) => void;
 }
 
-const Input = ({type, placeholder, label, value, onChange, className, style, ...props}: InputProps) => {
+const Input = ({placeholder, label, className, style, onChange, ...props}: InputProps) => {
   return (
     <View>
-      {label && <Text className="py-1 px-1 mb-1 text-theme-900">{label}</Text>}
+      {label && <Text className="py-1 px-1 mb-1 text-theme">{label}</Text>}
       <TextInput
-        className={twMerge('border border-greyish p-3 text-greyish-200 focus:border-theme', className)}
+        className={twMerge('border border-greyish-100 p-3 placeholder:text-greyish-100 focus:border-theme', className)}
         style={{borderRadius: 5, ...style}}
         placeholder={placeholder || ''}
-        value={value}
-        onChangeText={onChange ? text => onChange(text) : undefined}
+        onChangeText={onChange}
+        caretHidden={false}
         {...props}
       />
     </View>
