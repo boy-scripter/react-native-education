5d25ec16d2c61bc637b9f3ae841de44fa2940f1e
shivam <trickloot8.1@gmail.com>
2025-06-10 17:31:04 +0530

    moved to auth folder

--- src/stacks/Auth.stack.tsx
+++ src/stacks/Auth.stack.tsx
@@ -1,6 +1,6 @@
 import {createStackNavigator} from '@react-navigation/stack';
-import AuthScreen from '@screens/Auth.screen';
-import ForgotPasswordScreen from '@screens/ForgotPassword.screen';
+import AuthScreen from '@screens/auth/Auth.screen';
+import ForgotPasswordScreen from '@screens/auth/ForgotPassword.screen';
 
 const Stack = createStackNavigator();
 export function AuthStack() {
