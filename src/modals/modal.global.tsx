83c75dafd5969f441bd6089331efd9c8ba49861c
shivam <trickloot8.1@gmail.com>
2025-06-21 17:00:04 +0530

    modal code

--- src/modals/modal.global.tsx
+++ src/modals/modal.global.tsx
@@ -1,27 +1,64 @@
-import {createContext, ReactNode, useCallback, useContext, useState} from 'react';
-import Modal from  './Modal'
+import React, {Component, createContext, ReactNode, useCallback, useContext, useState} from 'react';
+import Modal from './Modal';
+import {id} from 'zod/v4/locales';
 
-interface ModalStructre {
-  id : string
-  component : React.ReactNode
+interface ModalStructure {
+  id: string;
+  component: React.FC<any>;
 }
 
-const ModalContext = createContext(null);
+const ModalContext = createContext<null | ReturnType<typeof useModalContextCreator>>(null);
+
+const useModalContextCreator = () => {
+  const [modalList, setModalList] = useState<ModalStructure[]>([]);
+
+  const getModalId = (component: React.FC<any>) => component.name;
+
+  function isModalExist(id: string) {
+    return modalList.some(modalsConfig => modalsConfig.id === id);
+  }
+
+  const open = useCallback((component: React.FC<any>) => {
+    const modalId = getModalId(component);
+
+    if (!isModalExist(modalId)) {
+      setModalList(state =>
+        state.concat({
+          id: modalId,
+          component,
+        }),
+      );
+    }
+  }, []);
+
+  const close = useCallback((identifier: string | React.FC<any>) => {
+    const modalId = typeof identifier != 'string' ? getModalId(identifier) : identifier;
+
+    if (isModalExist(modalId)) {
+      setModalList(state => state.filter(modal => modal.id !== modalId));
+    }
+  }, []);
+
+  return {close, open, modalList};
+};
+
+const ModalRenderer = () => {
+  
+}
+
 
 const ModalProvider = ({children}: {children: ReactNode}) => {
-  const [modalList, setModalList] = useState<ModalStructre[]>([]);
-
-
-  const open = useCallback((component : ReactNode) => {
-    const modalId = component
-    setModalList(prev => [])
-    }, []);
-  const close = useCallback(() => {}, []);
+  const {open, close, modalList} = useModalContextCreator();
 
   return (
-    <ModalContext.Provider value={(open, close)}>
-   
+    <ModalContext.Provider value={{open, close, modalList}}>
       {children}
+      {
+        modalList.map(
+          (currentModal, index) => {
+          return  <Modal key={index} component={currentModal.component}> </Modal>;
+        })
+      }
     </ModalContext.Provider>
   );
 };
