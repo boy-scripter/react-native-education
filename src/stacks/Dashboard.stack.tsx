5ffeab2dd15f65d67f33a5152e9b3185d584a042
shivam <trickloot8.1@gmail.com>
2025-07-31 23:38:45 +0530

    route chnages

--- src/stacks/Dashboard.stack.tsx
+++ src/stacks/Dashboard.stack.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from '@screens/dashboard/home.screen';
-import QuizScreen from '@screens/dashboard/quiz.screen';
-import LeaderBoardScreen from '@screens/dashboard/leaderboard.screen';
-import QuizInstructionScreen from '@screens/dashboard/quizInstruction.screen';
-import PdfViewScreen from '@/screens/dashboard/pdfview/pdfview.screen';
-import ResultScreen from '@screens/dashboard/result.screen';
+import HomeScreen from '@/screens/dashboard/home/home.screen';
+import PdfShowScreen from '@/screens/dashboard/pdfShow/pdfShow.screen';
+import PdfViewScreen from '@/screens/dashboard/pdfview';
 import EditProfileScreen from '@/screens/dashboard/EditProfile';
+// import QuizScreen from '@screens/dashboard/quiz.screen';
+// import LeaderBoardScreen from '@screens/dashboard/leaderboard.screen';
+// import QuizInstructionScreen from '@screens/dashboard/quizInstruction.screen';
+// import ResultScreen from '@screens/dashboard/result.screen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,9 +16,10 @@ const DashboardStack: React.FC = () => {
     <Stack.Navigator screenOptions={{headerShown: false, animation: 'slide_from_right', gestureEnabled: true}} initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+      <Stack.Screen name="PdfView" component={PdfShowScreen} />
+      <Stack.Screen name="PdfShow" component={PdfViewScreen} />
       {/* <Stack.Screen name="Quiz" component={QuizScreen} />
       <Stack.Screen name="QuizResult" component={QuizInstructionScreen} />
-      <Stack.Screen name="PdfView" component={PdfViewScreen} />
       <Stack.Screen name="Result" component={ResultScreen} />
       <Stack.Screen name="Leaderboard" component={LeaderBoardScreen} /> */}
     </Stack.Navigator>
--- src/types/navigation/dashboardstack/dashboardstack.interface.ts
+++ src/types/navigation/dashboardstack/dashboardstack.interface.ts
@@ -2,12 +2,13 @@ import { NavigatorScreenParams } from '@react-navigation/native';
 
 export type DashboardStackParamList = {
   Home: undefined;
+  EditProfile: undefined ;
+  PdfView: { url: string };
+  PdfShow: { category: string };
   Quiz: { quizId: string };
   QuizResult: { score: number; total: number };
-  PdfView: { url: string };
   Result: undefined;
   Leaderboard: { topicId: string };
-  EditProfile: undefined ;
 };
 
 export type DashboardStackNavigation = NavigatorScreenParams<DashboardStackParamList>;
