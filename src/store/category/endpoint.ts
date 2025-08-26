8ab190fc6fa68c5edd4f4124e67a56a040b6280f
shivam <trickloot8.1@gmail.com>
2025-08-26 22:11:55 +0530

    categoory cached

--- src/store/category/endpoint.ts
+++ src/store/category/endpoint.ts
@@ -1,7 +1,15 @@
 
 import { api } from "@/graphql/generated";
 
-export const categoryApi = api
 
-export const { useCategoriesQuery , useLazyCategoriesQuery  } = categoryApi;
-export type { CategoriesQuery  } from "@/graphql/generated";
+export const categoryApi = api.enhanceEndpoints({
+    addTagTypes: ['category'],
+    endpoints: {
+        Categories: {
+            keepUnusedDataFor: Infinity
+        }
+    }
+})
+
+export const { useCategoriesQuery, useLazyCategoriesQuery } = categoryApi;
+export type { CategoriesQuery } from "@/graphql/generated";
